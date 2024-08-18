e86d8af338dfedcd996db72f28224e7992a0c23f
KSJain <ksj@ksj.dev>
2024-08-18 15:09:09 -0400

    Initial Commit

---
+++ Fetch iOS Submission/Fetch_iOS_SubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_SubmissionApp.swift
+//  Fetch iOS Submission
+//
+//  Created by Kartikeya Saxena Jain on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_SubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
